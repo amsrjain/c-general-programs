a3c203c90ff4bc71945d1d14bb0f813caa44634f
amsrjain <83563136+amsrjain@users.noreply.github.com>
2021-05-04 00:45:05 -0700

    Added test call for matrix multiplication

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+// external functions
+
+// Matrix tests
+void matrix_test_1(void);
+void matrix_test_2(void);
+
+
 // Main function that calls sub functions for various trials
 int main(void) {
 	printf("Hello World! \n");
+
+	/* Multiply two matrix of size 2x3 and 3x4 */
+	matrix_test_1();
+
+	/* Multiply two matrix of size 6x3 and 3x9 */
+	matrix_test_2();
 
 	return 1;
 }
