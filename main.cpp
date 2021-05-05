5a78f9421148df8278a5c22746d41cc8e07e8d98
amsrjain <83563136+amsrjain@users.noreply.github.com>
2021-05-05 00:01:40 -0700

    Add support for String functions

--- main.cpp
+++ main.cpp
@@ -7,16 +7,28 @@
 void matrix_test_1(void);
 void matrix_test_2(void);
 
+void string_test_1(void);
+void string_test_2(void);
+void string_test_3(void);
 
 // Main function that calls sub functions for various trials
 int main(void) {
 	printf("Hello World! \n");
 
-	/* Multiply two matrix of size 2x3 and 3x4 */
+	/* test to Multiply two matrix of size 2x3 and 3x4 */
 	matrix_test_1();
 
-	/* Multiply two matrix of size 6x3 and 3x9 */
+	/* test to Multiply two matrix of size 6x3 and 3x9 */
 	matrix_test_2();
+
+	/* test if string has all unique characters */
+	string_test_1();
+
+	/* test if a string is a permutation of another */
+	string_test_2();
+
+	/* test if two strings are maximum one edit away */
+	string_test_3();
 
 	return 1;
 }
